d9869ce17db82ec9b8e48ec47a7927cefa908bbf
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-07-25 18:14:31 +0700

    Added: animator agent, collision and gravity behaviors.

--- UIkit Dynamics Demo/ViewController.swift
+++ UIkit Dynamics Demo/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Stored properties
+    
+    var greenBox: UIView?
+    var animator: UIDynamicAnimator?
+    var gravity: UIGravityBehavior?
+    var collision: UICollisionBehavior?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Instantiate the green box
+        self.greenBox = UIView()
+        self.greenBox?.backgroundColor = UIColor.greenColor()
+        self.greenBox?.frame = CGRect(x: CGRectGetMidX(self.view.frame) - 50, y: CGRectGetMidY(self.view.frame) - 50, width: 100, height: 100)
+        self.view.addSubview(self.greenBox!)
+        
+        // Instantiate the animation, gravity, & collision behavior
+        self.animator = UIDynamicAnimator(referenceView: self.view)
+        self.gravity = UIGravityBehavior(items: [self.greenBox!])
+        self.collision = UICollisionBehavior(items: [self.greenBox!])
+        self.collision?.translatesReferenceBoundsIntoBoundary = true
+            
+        // Pass the gravity & collision behavior to the animator
+        self.animator!.addBehavior(self.gravity!)
+        self.animator!.addBehavior(self.collision!)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
